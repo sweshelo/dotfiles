37822652793e329cf31d14cd60f50c556c632027
sweshelo <sweshelo@dmats97.xyz>
2021-11-18 12:22:35 +0000

    ファイルが存在する場合は退避してリンクを張る

--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# function
+function mapping(){
+    file_path=$1;
+    link_path=$2;
+
+    # Old config files evac.
+    if [ -e ${link_path} ]; then
+        mv ${link_path} ${link_path}.old
+    fi
+    
+    ln -s $file_path $link_path
+}
+
 # Vim
 if [ ! -d ~/.config/nvim ]; then
     mkdir -p ~/.config/nvim
     git clone https://github.com/sweshelo/nvimrc ~/.config/nvim/
 fi
 
+# Old config files evac.
+
 # Links
 SHELL_PATH=`pwd -P`
-ln -s ${SHELL_PATH}/bashrc ~/.bashrc
-ln -s ${SHELL_PATH}/bashrc ~/.bash_profile
-ln -s ${SHELL_PATH}/tmux.conf ~/.tmux.conf
+mapping ${SHELL_PATH}/bashrc ~/.bashrc
+mapping ${SHELL_PATH}/bashrc ~/.bash_profile
+mapping ${SHELL_PATH}/tmux.conf ~/.tmux.conf
