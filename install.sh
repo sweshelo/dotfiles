5d06f42a8ac334801cd2428e46730901ed442408
sweshelo <root@sweshelo.jp>
2022-02-09 17:49:11 +0900

    keymapに関する設定を追加

--- install.sh
+++ install.sh
@@ -30,3 +30,4 @@ SHELL_PATH=`pwd -P`
 mapping ${SHELL_PATH}/bashrc ~/.bashrc
 mapping ${SHELL_PATH}/bashrc ~/.bash_profile
 mapping ${SHELL_PATH}/tmux.conf ~/.tmux.conf
+mapping ${SHELL_PATH}/keymap.sh ~/.keymap.sh
